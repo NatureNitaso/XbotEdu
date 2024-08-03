12dd2aa73bf2c1d17bbd9159c176984c2c762ebe
NatureNitaso <86992928+NatureNitaso@users.noreply.github.com>
2024-08-03 11:56:10 -0700

    Sets up the check for x/y vals from the left joystick

--- src/main/java/competition/subsystems/drive/commands/ArcadeDriveWithJoysticksCommand.java
+++ src/main/java/competition/subsystems/drive/commands/ArcadeDriveWithJoysticksCommand.java
@@ -24,6 +24,15 @@ public class ArcadeDriveWithJoysticksCommand extends BaseCommand {
 
     @Override
     public void execute() {
+        // Tracks the x and y position of the joystick and determines the course of action to take
+        double xVal = operatorInterface.gamepad.getLeftStickX();
+        double yVal = operatorInterface.gamepad.getLeftStickY();
+
+        // Checks for the vals before determining action
+        if (xVal > 0 && yVal == 0)
+        {
+            drive.tankDrive(xVal, xVal);
+        }
     }
 
 }
